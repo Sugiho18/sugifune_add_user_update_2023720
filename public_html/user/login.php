2c3d92edce25a6d0e0cdae1aaa45d07804a57ea8
Sugiho18 <sugifune@se-sendai.co.jp>
2023-07-24 13:49:39 +0900

    アカウント編集機能追加

--- public_html/user/login.php
+++ public_html/user/login.php
@@ -1,16 +1,14 @@
 <?php
+require_once '../../bootstrap.php';
+require_once ROOT_DIR . '/app/Controllers/UserController.php';
+$ctrl = new UserController();
+extract($ctrl->login());
 
 /**
  * ログイン画面
  * 
  * @since 1.0.0
  */
-require_once '../../bootstrap.php';
-require_once ROOT_DIR . '/app/Controllers/UserController.php';
-
-$ctrl = new UserController();
-extract($ctrl->login());
-
 $title = 'ログイン';
 ?>
 
